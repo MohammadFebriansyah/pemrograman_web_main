965967d8e5ad440261b411f35ce218f692876506
MohammadFebriansyah <mohammadfebriansyah142@gmail.com>
2025-09-26 06:57:37 +0700

    js 5 praktikum 1 soal 2

--- fungsi.php
+++ fungsi.php
@@ -1,11 +1,16 @@
 <?php
-
-function perkenalan(){
-    echo "Assalamualaikum, ";
-    echo "Perkenalkan, nama saya Febri<br/>"; //Tulis sesuai nama kalian
+function perkenalan($nama, $salam) {
+    echo $salam.", ";
+    echo "Perkenalkan, nama saya ".$nama."<br/>";
     echo "Senang berkenalan dengan Anda<br/>";
 }
-//memanggil fungsi yang sudah dibuat
-perkenalan();
 
+perkenalan ("Ansyah","Hallo");
+
+echo "<hr>";
+
+$saya = "Febri";
+$ucapanSalam = "Selamat pagi";
+
+perkenalan($saya, $ucapanSalam);
 ?>
