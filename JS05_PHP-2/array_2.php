fe6b01234eea7c1f80a5e28aae81128d0d3342d9
MohammadFebriansyah <mohammadfebriansyah142@gmail.com>
2025-09-27 16:23:42 +0700

    js 5.2 praktikum 2

---
+++ JS05_PHP-2/array_2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+</head>
+<body>
+    <?php
+        $Dosen = [
+            'nama' => 'Mohammad Febriansyah',
+            'domisili' => 'Blitar',
+            'jenis_kelamin' => 'Laki-laki' ];
+
+        echo "Nama : {$Dosen ['nama' ]} <br>";
+        echo "Domisili : {$Dosen ['domisili' ]} <br>";
+        echo "Jenis Kelamin : {$Dosen ['jenis_kelamin']} <br>";
+    ?>
+</body>
+</html
