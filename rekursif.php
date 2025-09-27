6da8d21a0de704aa0a77c9810e749e65f2e1bb60
MohammadFebriansyah <mohammadfebriansyah142@gmail.com>
2025-09-27 11:04:19 +0700

    js 5 praktikum 1 soal 5.5

--- rekursif.php
+++ rekursif.php
@@ -1,8 +1,5 @@
 <?php
-function tampilkanHaloDunia(){
-    echo "Hallo Dunia <br>";
-
-    tampilkanHaloDunia();
+for ($i=1; $i <= 25; $i++){
+    echo "Perulangan ke-{$i} <br>";
 }
-tampilkanHaloDunia();
 ?>
