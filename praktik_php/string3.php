ac75719fd17e8f4b2e9fd456e8bb5e76614f4837
MohammadFebriansyah <mohammadfebriansyah142@gmail.com>
2025-10-02 19:17:27 +0700

    js 6.1 praktikum soal 16

--- praktik_php/string3.php
+++ praktik_php/string3.php
@@ -1,4 +1,7 @@
 <?php
-$pesan = "Saya arek malang";
-echo strrev($pesan)."<br>";
+$pesan = "saya arek malang";
+$pesanPerkata = explode(" ", $pesan); 
+$pesanPerkata = array_map(fn($pesan) => strrev($pesan), $pesanPerkata);
+$pesan = implode(" ", $pesanPerkata);
+echo $pesan. "<br>";
 ?>
