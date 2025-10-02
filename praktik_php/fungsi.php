396be8496a491e409f3df6eec46164022c17fba8
MohammadFebriansyah <mohammadfebriansyah142@gmail.com>
2025-10-02 18:10:00 +0700

    js 6.1 praktikum soal 4

---
+++ praktik_php/fungsi.php
@@ -0,0 +1,10 @@
+<?php // SOAL 4
+function perkenalan(){
+    echo "Assalamualaikum, ";
+    echo "Perkenalkan, nama saya Febri<br/>"; 
+    echo "Senang berkenalan dengan anda<br/>";
+}
+    perkenalan();
+    perkenalan()
+?>
+
