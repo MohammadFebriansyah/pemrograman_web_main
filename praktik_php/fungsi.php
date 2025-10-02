2a56aab2a379cd0c50dc5f441921d0f078be9c42
MohammadFebriansyah <mohammadfebriansyah142@gmail.com>
2025-10-02 18:20:32 +0700

    js 6.1 praktikum soal 7

--- praktik_php/fungsi.php
+++ praktik_php/fungsi.php
@@ -1,16 +1,8 @@
 <?php 
-function perkenalan ($nama, $salam="Assalamualaikum") {
-    echo $salam.", ";
-    echo "Perkenalkan, nama saya ".$nama."<br/>";
-    echo "Senang berkenalan dengan Anda<br/>";
+function hitungUmur($thn_lahir, $thn_sekarang){
+    $umur = $thn_sekarang - $thn_lahir;
+    return $umur;
 }
 
-perkenalan ("Ansyah", "Hallo");
-
-echo "<hr>";
-
-$saya = "Febri";
-$ucapanSalam = "Selamat pagi";
-
-perkenalan ($saya);
+echo "Umur saya adalah ".hitungUmur(2004, 2025)." tahun"
 ?>
