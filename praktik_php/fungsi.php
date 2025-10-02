cd59c53cf1ec89a2bccfa862c2a45734510bdc68
MohammadFebriansyah <mohammadfebriansyah142@gmail.com>
2025-10-02 18:51:41 +0700

    js 6.1 praktikum soal 8

--- praktik_php/fungsi.php
+++ praktik_php/fungsi.php
@@ -1,8 +1,13 @@
 <?php 
-function hitungUmur($thn_lahir, $thn_sekarang){
-    $umur = $thn_sekarang - $thn_lahir;
-    return $umur;
+    function hitungUmur($thn_lahir, $thn_sekarang) {
+        $umur = $thn_sekarang - $thn_lahir;
+        return $umur;
 }
-
-echo "Umur saya adalah ".hitungUmur(2004, 2025)." tahun"
+function perkenalan($nama, $salam="Assalamualaikum"){
+    echo $salam. ",";
+    echo " Perkenalkan, nama saya ".$nama."<br/>"; 
+    echo "Saya beerusia ". hitungUmur(2004, 2025) ." tahun<br/>";
+    echo "Senang berkenalan dengan anda<br/>";
+}
+    perkenalan("Febri");
 ?>
