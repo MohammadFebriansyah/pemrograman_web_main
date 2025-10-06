06b707981f2cff3519c6a4be23dccbc92c5e53c6
MohammadFebriansyah <mohammadfebriansyah142@gmail.com>
2025-10-07 04:19:28 +0700

    js 7 praktikum 2 soal 2.2

--- empty.php
+++ empty.php
@@ -6,3 +6,11 @@ if (empty($myArray)) {
     echo "Array terdefinisi dan tidak kosong.<br>";
 }
 ?>
+
+<?php 
+if (empty($nonExistentVar)) { 
+    echo "Variabel tidak terdefinisi atau kosong.";
+} else { 
+    echo "Variabel terdefinisi dan tidak kosong.";
+}
+?>
