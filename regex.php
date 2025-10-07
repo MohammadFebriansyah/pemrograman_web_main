122a13f269238779769283c3d89c02493d5d8242
MohammadFebriansyah <mohammadfebriansyah142@gmail.com>
2025-10-07 11:49:39 +0700

    js 7 praktikum 5 soal 5.2

--- regex.php
+++ regex.php
@@ -8,3 +8,14 @@ if (preg_match($pattern, $text)) {
 }
 echo "<br>";
 ?>
+
+<?php // kode tambahan pada langkah 6
+$pattern = '/[0-9]+/'; // Cocokkan satu atau lebih digit.
+$text = 'This are 123 apples.';
+if (preg_match($pattern, $text, $matches)) { 
+    echo "Cocokkan: " . $matches[0];
+} else { 
+    echo "Tidak ada yang cocok!";
+}
+echo "<br>";
+?>
