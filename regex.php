5a62952f825c422678f98168125182f382657842
MohammadFebriansyah <mohammadfebriansyah142@gmail.com>
2025-10-07 11:57:01 +0700

    js 7 praktikum 5 soal 5.5

--- regex.php
+++ regex.php
@@ -39,3 +39,14 @@ if (preg_match($pattern, $text, $matches)) {
 }
 echo "<br>";
 ?>
+
+<?php // soal 5.5
+$pattern = '/go?d/'; // Cocokkan "god" atau "good".
+$text = 'god is good.';
+if (preg_match($pattern, $text, $matches)) { 
+    echo "Cocokkan: " . $matches[0];
+} else {
+    echo "Tidak ada yang cocok!";
+}
+echo "<br>";
+?>
