a8a3e9bfa3753ffbb090ce377ba3afe241ee4451
MohammadFebriansyah <mohammadfebriansyah142@gmail.com>
2025-10-23 13:49:21 +0700

    js 8 praktikum 1 soal 3&4

--- Jobsheet-8/upload.php
+++ Jobsheet-8/upload.php
@@ -1,13 +1,20 @@
 <?php
-if(isset($_POST["submit"])){
-    $targetdir = "uploads/"; //Direktori tujuan untuk menyimpan file
-    $targetfile = $targetdir . basename($_FILES["myfile"]["name"]);
-
-    if(move_uploaded_file($_FILES["myfile"]["tmp_name"], $targetfile) ){
-    echo "File berhasil diunggah.";
-    }
-    else{
-    echo "Gagal mengunggah file.";
+ if (isset($_POST["submit"])) {
+    $targetDirectory = "uploads/"; // Direktori tujuan untuk menyimpan file
+    $targetFile = $targetDirectory . basename($_FILES["myfile"]["name"]); 
+    $fileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
+    
+    $allowedExtensions = array("jpg", "jpeg", "png", "gif");
+    $maxSize = 5* 1024* 1024;
+    
+    if (in_array($fileType, $allowedExtensions) && $_FILES["myfile"]["size"] <= $maxSize) { 
+        if (move_uploaded_file($_FILES["myfile"]["tmp_name"], $targetFile)) { 
+            echo "File berhasil diunggah.";
+        } else {
+            echo "Gagal mengunggah file.";
+        }
+    } else {
+        echo "File tidak valid atau melebihi ukuran maksimum yang diizinkan.";
     }
 }
 ?>
