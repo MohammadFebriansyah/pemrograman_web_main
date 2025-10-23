39e4ca19f3e5a9f4ac16acafcbe29979870bcb23
MohammadFebriansyah <mohammadfebriansyah142@gmail.com>
2025-10-23 17:37:21 +0700

    js 8 praktikum 2 soal 9

--- Jobsheet-8/proses_upload.php
+++ Jobsheet-8/proses_upload.php
@@ -1,29 +1,30 @@
 <?php
 // Lokasi penyimpanan file yang diunggah
-$targetDirectory = "uploads/";
+$targetDirectory = "upload/";
 
 // Periksa apakah direktori penyimpanan ada, jika tidak maka buat
 if (!file_exists($targetDirectory)) {
     mkdir($targetDirectory, 0777, true);
 }
 
-if ($_FILES['files'] && $_FILES['files']['name'][0]) {
-    $totalFiles = count($_FILES['files']['name']);
-    
-    // Loop melalui semua file yang diunggah
+if ($_FILES['images'] && $_FILES['images']['name'][0]) {
+    $totalFiles = count($_FILES['images']['name']);
+
+    // Loop melalui semua gambar yang diunggah
     for ($i = 0; $i < $totalFiles; $i++) {
-        $fileName = $_FILES['files']['name'][$i];
+        $fileName = $_FILES['images']['name'][$i];
         $targetFile = $targetDirectory . $fileName;
-        // Pindahkan file yang diunggah ke direktori penyimpanan
-        if (move_uploaded_file($_FILES['files']['tmp_name'][$i], $targetFile)) {
-            echo "File $fileName berhasil diunggah.<br>";
+        
+        // Pindahkan gambar yang diunggah ke direktori penyimpanan
+        if (move_uploaded_file($_FILES['images']['tmp_name'][$i], $targetFile)) {
+            echo "Gambar $fileName berhasil diunggah.<br>";
         } 
         else {
-            echo "Gagal mengunggah file $fileName.<br>";
+            echo "Gagal mengunggah gambar $fileName.<br>";
         }
     }
 } 
 else {
-    echo "Tidak ada file yang diunggah.";
+    echo "Tidak ada gambar yang diunggah.";
 }
 ?>
