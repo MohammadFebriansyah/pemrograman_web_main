b35e3660631b6949a0c6cf36b8b7960d62b9b92b
MohammadFebriansyah <mohammadfebriansyah142@gmail.com>
2025-10-23 16:14:00 +0700

    js 8 praktikum 1 soal 6&7

--- Jobsheet-8/upload.php
+++ Jobsheet-8/upload.php
@@ -4,13 +4,12 @@
     $targetFile = $targetDirectory . basename($_FILES["myfile"]["name"]); 
     $fileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
     
-    $allowedExtensions = array("jpg", "jpeg", "png", "gif");
-    $maxSize = 5* 1024* 1024;
+    $allowedExtensions = array("txt", "pdf", "doc", "docx");
+    $maxSize = 3* 1024* 1024;
     
     if (in_array($fileType, $allowedExtensions) && $_FILES["myfile"]["size"] <= $maxSize) { 
         if (move_uploaded_file($_FILES["myfile"]["tmp_name"], $targetFile)) { 
             echo "File berhasil diunggah.";
-            echo '<br><br><img src="' . $targetFile . '" width="200" alt="Thumnail">';
         } else {
             echo "Gagal mengunggah file.";
         }
